8404201942c88c3b345d209a73b2df640d7c9374
Diego Filastro <d.filastro@hotmail.com>
2023-07-04 16:33:16 -0300

    feat: added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
 
         {children}
       </body>
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 8rem;
+`;
+
+const Logo = styled.img``;
+
+export default function Header() {
+  return (
+    <Container>
+      <Logo src='logo.png' />
+    </Container>
+  );
+}
